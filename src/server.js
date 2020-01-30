6d6119a10ec5938a17699698c4b1cd67af02b31c
Adomas Skliutas <adomas.skliutas@instarem.com>
2020-01-30 16:56:42 +0200

    added cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const { db } = require('./mongoose');
 const { loginUser, registerUser } = require('./controllers/user.controller');
@@ -10,6 +11,7 @@ const { authenticationMiddleware } = require('./auth.middleware');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/login', async (req, res, next) => {
 	try {
